3d1a2c338cc34055b571f004be9ddbcbbca14ed2
Renan Lebrão <renan.lebrao@outlook.com>
2026-02-11 15:05:34 -0300

    11/02

---
+++ Aula 1/script.js
@@ -0,0 +1,16 @@
+var nome = "Renan"
+
+var idade = 18;
+
+var altura = 1.87;
+
+console . log (nome);
+console . log (idade);
+console . log ( altura);
+
+document .write ("<h1>olá, seja bem vindo ao javascript ")
+
+document . write ( nome + "<br>" );
+document . write ( idade + "<br>" )
+document . write ( altura + "<br>" );
+document . write "< p style= color : red > "
