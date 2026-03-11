0a32268527bbd4180d69a596f6d97fe117dd838c
Renan Lebrão <renan.lebrao@outlook.com>
2026-03-11 14:58:19 -0300

    11/03

--- Aula 1/exercicio1.js
+++ Aula 1/exercicio1.js
@@ -1,10 +1,10 @@
-document.write ("<h1> Exercicio 1</h1>")
+document.write ("h1 style='color : red'> Aula 01 - Exercicio1 </h1>")
 
-var numero = prompt ("Digite um Numero qualquer:")
-var quadrado = Number (numero) * Number (numero) 
-var cubo = Number (numero) * Number (numero) * Number (numero)
+var numero = prompt ("Digite Um numero qualquer:");
+document.write ("<p> O numero digitado foi" + numero + "<p>");
 
+var quadrado = Number (numero) * Number (numero);
+document.write ("<p>O quadrado do numero e digitado é " + quadrado + "<p>");
 
-document.write ("<p>O numero digitado é " + numero + "</p>")
-document.write ("<p>O quadrado do numero é " + quadrado + "</p>")
-document.write ("<p> o cubo do numero é " + cubo+ "</p>")
+var cubo = Number (numero) * Number (numero) * Number (numero);
+document.write ("<p>O cubo do número digitado é " + cubo + "<p>")
---
+++ Aula 1/exercicio2.js
@@ -0,0 +1,5 @@
+document.write ("Exercicio 2")
+var temp = prompt("Digite a temperatura em Farenheit");
+var celsius = (Number(temp) - 32) * 5 / 9;
+
+document.write ("<p> A temperatura é igual a:" + celsius , "°c</p>")
