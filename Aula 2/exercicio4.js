6fea5ced66b36aa65e6827b2a0573489fb6b7e49
Renan Lebrão <renan.lebrao@outlook.com>
2026-04-08 14:59:45 -0300

    08/04

---
+++ Aula 2/exercicio4.js
@@ -0,0 +1,15 @@
+function calcular ()
+{
+    let bt, at, area; 
+
+
+
+    bt = Number (document.getElementById("bt").value);
+    at = Number (document.getElementById("at").value);
+
+    area= bt*at;
+
+document.getElementById ("resultado").innerHTML = "<p> O valor da area do Triangulo é" + resultado + "<p>"
+
+}
+
